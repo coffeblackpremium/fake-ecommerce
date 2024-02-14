a490b2e302332c3327c0a83be384d453c84422e2
Joao Damaceno <joaodamaceno188@gmail.com>
2024-02-14 01:47:56 -0400

    feat: configura banco de produtos.

---
+++ database/migrations/2024_02_12_223838_create_products_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('category');
+            $table->string('image');
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+};
